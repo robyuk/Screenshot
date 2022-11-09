57c4eb7f89d9fc3dcd43b91105f36a3b413f5876
robertay <94378311+robyuk@users.noreply.github.com>
2022-11-09 10:36:16 +0000

    Changed to capture part of the screen to a png file

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 #! /usr/bin/env /usr/bin/python3
 #
-# This script captures a screenshot to outfile every 10 minutes when run on your PC
+# This script captures a partial screenshot to outfile.
 # On an Apple Mac you may have to allow access for this script to access screen recording
 #
 #  DOES NOT WORK ON REPLIT because it cannot acces a screen there.
 #
 # Pull this from git at https://github.com/robyuk/Screenshot to your PC, then install mss on your pc with "pip install mss"
-#from mss import mss
+from mss import mss, tools
 from time import sleep, strftime
 
 outfileprefix='screenshot'
 outfilext='.png'
+part={'top':57, 'left':23, 'height':600, 'width':400} # Values are in pixels
 
-while True:
-  timestamp=strftime('%Y%m%d%H%M%S')
-  with mss() as screen:
-    screen.shot(output=f'{outfileprefix}{timestamp}{outfilext}')
-  print(f'{outfileprefix}{timestamp}{outfilext}')
-  sleep(600)
+timestamp=strftime('%Y%m%d%H%M%S')
+outfile=f'{outfileprefix}{timestamp}{outfilext}'
+with mss() as screen:
+  image=screen.grab(part)
+  tools.to_png(image.rgb, image.size, output=outfile)
+print(outfile)
