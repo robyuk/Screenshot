2587cc20b89df57c3063f33202e6fa7e78557c44
robertay <94378311+robyuk@users.noreply.github.com>
2022-11-09 10:15:28 +0000

    Added loop to capture every 10 minutes

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 #! /usr/bin/env /usr/bin/python3
 #
-# This script captures a screenshot to outfile when run on your PC
+# This script captures a screenshot to outfile every 10 minutes when run on your PC
 # On an Apple Mac you may have to allow access for this script to access screen recording
 #
 #  DOES NOT WORK ON REPLIT because it cannot acces a screen there.
 #
 # Pull this from git at https://github.com/robyuk/Screenshot to your PC, then install mss on your pc with "pip install mss"
-from mss import mss
+#from mss import mss
+from time import sleep, strftime
 
-outfile='screenshot.png'
+outfileprefix='screenshot'
+outfilext='.png'
 
-with mss() as screen:
-  screen.shot(output=outfile)
+while True:
+  timestamp=strftime('%Y%m%d%H%M%S')
+  #with mss() as screen:
+    #screen.shot(output=f'{outfileprefix}{timestamp}{outfilext}')
+  print(f'{outfileprefix}{timestamp}{outfilext}')
+  sleep(600)
